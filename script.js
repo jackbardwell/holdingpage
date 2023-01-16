5d6ecc9356b0cf84b46096148fafde543f41c5f1
Jack Bardwell <jack@jackbardwell.com>
2023-01-16 15:19:55 +0100

    Moving Update

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-dragElement(document.getElementById("center"));
+window.onload = function() {
+    dragElement(document.getElementById("image"));
+  };
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "center")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "center").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+  elmnt.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -39,3 +35,4 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+}
